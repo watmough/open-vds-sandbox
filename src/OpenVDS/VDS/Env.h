434c3126730a24eb96459961311217e176bb3ab1
Jørgen Lind <jorgen.lind@bluware.com>
2021-09-06 12:33:10 +0200

    Move CA autodetection into library code

--- src/OpenVDS/VDS/Env.h
+++ src/OpenVDS/VDS/Env.h
@@ -1,9 +1,15 @@
 #ifndef ENV_H
 #define ENV_H
 
+#include <string>
+
 namespace OpenVDS
 {
 bool getBooleanEnvironmentVariable(const char *name);
+
+bool isEnvironmentVariableSet(const char* name);
+
+void setEnvironmentVariable(const char *name, const std::string &value);
 }
 
 #endif
