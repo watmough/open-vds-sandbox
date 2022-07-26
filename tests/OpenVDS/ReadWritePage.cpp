0924daea253e49e530cfb2f7e97c9af37151b3f5
morten.ofstad <morten.ofstad@bluware.com>
2022-07-26 13:02:51 +0200

    Added TestVolumeDataReadAccessorLOD to verify that VolumeDataAccessors handle LOD and full-resolution dimensions correctly.

--- tests/OpenVDS/ReadWritePage.cpp
+++ tests/OpenVDS/ReadWritePage.cpp
@@ -604,3 +604,452 @@ TEST(OpenVDS_integration, RemapLOD)
     accessManager.DestroyVolumeDataPageAccessor(pageAccessor);
   }
 }
+
+/*
+class VolumeDataAccessorLODTestPlugin : public Hue::HueSpaceLib::VDSFilterPlugin
+{
+private:
+  int m_fullResolutionDimension;
+
+public:
+  static Hue::HueSpaceLib::PluginParameterDescriptor
+  DescribePluginParameter(int parameter)
+  {
+    static Hue::HueSpaceLib::PluginParameterDescriptor
+      descriptors[] = {
+        Hue::HueSpaceLib::PluginParameterDescriptor::PluginParameterInt("SizeT", "Size T", "Size in T dimension (dimension 3)", 10),
+        Hue::HueSpaceLib::PluginParameterDescriptor::PluginParameterInt("SizeI", "Size I", "Size in I dimension (dimension 2)", 10),
+        Hue::HueSpaceLib::PluginParameterDescriptor::PluginParameterInt("SizeJ", "Size J", "Size in J dimension (dimension 1)", 10),
+        Hue::HueSpaceLib::PluginParameterDescriptor::PluginParameterInt("SizeK", "Size K", "Size in K dimension (dimension 0)", 10),
+        Hue::HueSpaceLib::PluginParameterDescriptor::PluginParameterInt("Dimensionality", "Dimensionality", "Number of dimensions", 4),
+        Hue::HueSpaceLib::PluginParameterDescriptor::PluginParameterInt("FullResolutionDimension", "Full-resolution dimension", "Full-resolution dimension, if any", -1),
+        Hue::HueSpaceLib::PluginParameterDescriptor::PluginParameterEnd()
+      };
+
+    return descriptors[parameter];
+  }
+
+  bool
+  CanProduceLOD(int lod) override
+  {
+    return true;
+  }
+
+  int
+  GetFullResolutionDimension() override
+  {
+    return m_fullResolutionDimension;
+  }
+
+  Hue::HueSpaceLib::InputChannelDescriptor
+  DescribeInputChannel(int channel) override
+  {
+    return Hue::HueSpaceLib::InputChannelDescriptor::InputChannelEnd();
+  }
+
+  Hue::HueSpaceLib::IndexingDescriptor
+  DescribeInputIndexingMethod(int inputVDS, int dimension) override
+  {
+    return IndexingDescriptorEnd();
+  }
+
+  int
+  GetOutputDimensionality(Hue::HueSpaceLib::AccessVDSPluginParameterInterface &parameterInterface) override
+  {
+    return std::min(std::max(parameterInterface.GetValueInt("Dimensionality"), 2), 4);
+  }
+
+  Hue::HueSpaceLib::VolumeDataAxisDescriptor
+  DescribeOutputAxis(int dimension, Hue::HueSpaceLib::AccessVDSPluginParameterInterface &parameterInterface) override
+  {
+    switch (dimension)
+    {
+    case 0: return Hue::HueSpaceLib::VolumeDataAxisDescriptor(parameterInterface.GetValueInt("SizeK"), "K", "", 1.0, float(parameterInterface.GetValueInt("SizeK")));
+    case 1: return Hue::HueSpaceLib::VolumeDataAxisDescriptor(parameterInterface.GetValueInt("SizeJ"), "J", "", 1.0, float(parameterInterface.GetValueInt("SizeJ")));
+    case 2: return Hue::HueSpaceLib::VolumeDataAxisDescriptor(parameterInterface.GetValueInt("SizeI"), "I", "", 1.0, float(parameterInterface.GetValueInt("SizeI")));
+    case 3: return Hue::HueSpaceLib::VolumeDataAxisDescriptor(parameterInterface.GetValueInt("SizeT"), "T", "", 1.0, float(parameterInterface.GetValueInt("SizeT")));
+    }
+
+    return Hue::HueSpaceLib::VolumeDataAxisDescriptor();
+  }
+
+  int
+  GetOutputChannelCount(Hue::HueSpaceLib::AccessVDSPluginParameterInterface &parameterInterface) override
+  {
+    return 1;
+  }
+
+  Hue::HueSpaceLib::ProcessorPreference
+  GetOutputChannelProcessorPreference(Hue::HueSpaceLib::AccessVDSPluginParameterInterface &parameterInterface, int channel)
+  {
+    return Hue::HueSpaceLib::ProcessorPreferenceCPU;
+  }
+
+  static int
+  VoxelValue(intVec<4> voxel)
+  {
+    return voxel[0] + 37 * (voxel[1] + 37 * (voxel[2] + 37 * voxel[3]));
+  }
+
+  Hue::HueSpaceLib::VolumeDataChannelDescriptor
+  DescribeOutputValue(Hue::HueSpaceLib::AccessVDSPluginParameterInterface &parameterInterface, int channel) override
+  {
+    int
+      dimensionality = GetOutputDimensionality(parameterInterface);
+
+    intVec<4>
+      voxelIndex(parameterInterface.GetValueInt("SizeK") - 1,
+                 parameterInterface.GetValueInt("SizeJ") - 1,
+                 dimensionality >= 3 ? parameterInterface.GetValueInt("SizeI") - 1 : 0,
+                 dimensionality >= 4 ? parameterInterface.GetValueInt("SizeT") - 1 : 0);
+
+    return Hue::HueSpaceLib::VolumeDataChannelDescriptor(Hue::HueSpaceLib::DataBlock::Format_R32, Hue::HueSpaceLib::DataBlock::Components_1, "Value", "", 0.0f, (float)VoxelValue(voxelIndex));
+  }
+
+  void
+  OnPluginParametersCreated(Hue::HueSpaceLib::AccessVDSPluginParameterInterface &parameterInterface) override
+  {
+    m_fullResolutionDimension = parameterInterface.GetValueInt("FullResolutionDimension");
+  }
+
+  /// Called when host application has changed one or more of the plugin parameters
+  /// Member variables of the plugin should be set here, since this function will be called for each instance of the plugin.
+  /// \param parameterInterface The parameter interface for accessing plugin parameters and input metadata.
+  /// \return The action to take after parameters have changed
+  ParametersChangedAction
+  OnPluginParametersChanged(Hue::HueSpaceLib::AccessVDSPluginParameterInterface &parameterInterface) override
+  {
+    OnPluginParametersCreated(parameterInterface);
+    return Continue_Processing;
+  }
+
+  void
+  ProcessFilter(Hue::HueSpaceLib::VolumeDataCacheItem **, int, const Hue::HueSpaceLib::VolumeDataCacheItem **, int, Context *context) override
+  {
+    Hue::HueSpaceLib::VolumeDataCacheItem
+      *outputItem = context->GetOutputVolumeDataCacheItem(0);
+
+    Hue::HueSpaceLib::DataBlock
+      *valuesDataBlock  = outputItem->CreateDataBlock();
+
+    VolumeIndexer4D
+      valuesIndexer(outputItem, valuesDataBlock);
+
+    float
+      *values = valuesDataBlock->GetBufferR32CPU();
+
+    int
+      samples[4] = { valuesIndexer.GetDataBlockNumSamples(0),
+                     valuesIndexer.GetDataBlockNumSamples(1),
+                     valuesIndexer.GetDataBlockNumSamples(2),
+                     valuesIndexer.GetDataBlockNumSamples(3) };
+
+    for (int dim3 = 0; dim3 < samples[3]; ++dim3)
+    for (int dim2 = 0; dim2 < samples[2]; ++dim2)
+    for (int dim1 = 0; dim1 < samples[1]; ++dim1)
+    for (int dim0 = 0; dim0 < samples[0]; ++dim0)
+    {
+      intVec<4>
+        localIndex(dim0, dim1, dim2, dim3),
+        voxelIndex = valuesIndexer.LocalIndexToVoxelIndex(localIndex);
+
+      values[valuesIndexer.LocalIndexToDataIndex(localIndex)] = (float)VoxelValue(voxelIndex);
+    }
+  }
+};
+
+Hue::HueSpaceLib::PluginFactory &
+GetVolumeDataAccessorLODTestPluginFactoryInstance()
+{
+  static Hue::HueSpaceLib::PluginFactoryTemplate<VolumeDataAccessorLODTestPlugin>
+    instance("HueVolumeDataAccessorLODTestPlugin", "VolumeDataAccessorLODTestPlugin", "VolumeDataAccessorLODTestPlugin");
+
+  return instance;
+}
+*/
+
+void FillPages(OpenVDS::VolumeDataPageAccessor *pageAccessor, std::function<float(OpenVDS::IntVector4)> voxelValueFunction)
+{
+  int chunkCount = int(pageAccessor->GetChunkCount());
+  auto channelDescriptor = pageAccessor->GetChannelDescriptor();
+  if(channelDescriptor.GetFormat() != OpenVDS::VolumeDataFormat::Format_R32) throw std::runtime_error("FillVDS can only create R32 data for now");
+
+  for (int i = 0; i < chunkCount; i++)
+  {
+    OpenVDS::VolumeDataPage *page =  pageAccessor->CreatePage(i);
+
+    int min[OpenVDS::VolumeDataLayout::Dimensionality_Max];
+    int max[OpenVDS::VolumeDataLayout::Dimensionality_Max];
+    page->GetMinMax(min, max);
+
+    int LOD = pageAccessor->GetLOD();
+
+    int size[OpenVDS::Dimensionality_Max];
+    int pitch[OpenVDS::Dimensionality_Max];
+    void *buffer = page->GetWritableBuffer(size, pitch);
+
+    auto layoutDescriptor = pageAccessor->GetLayout()->GetLayoutDescriptor();
+    int fullResolutionDimension = layoutDescriptor.IsForceFullResolutionDimension() ? layoutDescriptor.GetFullResolutionDimension() : -1;
+    int dimensionLOD[4] = { fullResolutionDimension != 0 ? LOD : 0,
+                            fullResolutionDimension != 1 ? LOD : 0,
+                            fullResolutionDimension != 2 ? LOD : 0,
+                            fullResolutionDimension != 3 ? LOD : 0 };
+
+    for (int t = 0; t < size[3]; t++)
+    for (int i = 0; i < size[2]; i++)
+    for (int j = 0; j < size[1]; j++)
+    for (int k = 0; k < size[0]; k++)
+    {
+      OpenVDS::IntVector4 voxel(min[3] + (t << dimensionLOD[3]),
+                                min[2] + (i << dimensionLOD[2]),
+                                min[1] + (j << dimensionLOD[1]),
+                                min[0] + (k << dimensionLOD[0]));
+
+      int offset = k + j * pitch[1] + i * pitch[2] + t * pitch[3];
+
+      OpenVDS::WriteElement((float *)buffer, offset, voxelValueFunction(voxel));
+    }
+
+    page->Release();
+  }
+  pageAccessor->Commit();
+}
+
+static int
+LODTestVoxelValue(OpenVDS::IntVector4 voxel)
+{
+  return voxel[3] + 37 * (voxel[2] + 37 * (voxel[1] + 37 * voxel[0]));
+}
+
+OpenVDS::VDS *CreateVolumeDataAccessorLODTestVDS(int dimensionality, int fullResolutionDimension)
+{
+  const int
+    SIZE_K = 79,
+    SIZE_J = 69,
+    SIZE_I = 37,
+    SIZE_T = 7;
+
+  OpenVDS::Error error;
+  int negativeMargin = 4;
+  int positiveMargin = 4;
+  int brickSize2DMultiplier = 4;
+  auto lodLevels = OpenVDS::VolumeDataLayoutDescriptor::LODLevels_4;
+  auto layoutOptions = OpenVDS::VolumeDataLayoutDescriptor::Options_Create2DLODs;
+
+  if(fullResolutionDimension >= 0)
+  {
+    layoutOptions = layoutOptions | OpenVDS::VolumeDataLayoutDescriptor::Options_ForceFullResolutionDimension;
+  }
+
+  OpenVDS::VolumeDataLayoutDescriptor layoutDescriptor(OpenVDS::VolumeDataLayoutDescriptor::BrickSize_32, negativeMargin, positiveMargin, brickSize2DMultiplier, lodLevels, layoutOptions, fullResolutionDimension);
+
+  std::vector<OpenVDS::VolumeDataAxisDescriptor> axisDescriptors;
+  axisDescriptors.emplace_back(SIZE_K, "X", "", 1.0f, float(SIZE_K));
+  axisDescriptors.emplace_back(SIZE_J, "Y", "", 1.0f, float(SIZE_J));
+  if(dimensionality > 2) axisDescriptors.emplace_back(SIZE_I, "Z", "", 1.0f, float(SIZE_I));
+  if(dimensionality > 3) axisDescriptors.emplace_back(SIZE_T, "T", "", 1.0f, float(SIZE_T));
+
+  std::vector<OpenVDS::VolumeDataChannelDescriptor> channelDescriptors;
+  float rangeMin = 0.0f;
+  float rangeMax = 1.0f;
+  float integerScale = 0;
+  float integerOffset = 0;
+  channelDescriptors.push_back(OpenVDS::VolumeDataChannelDescriptor(OpenVDS::VolumeDataChannelDescriptor::Format_R32, OpenVDS::VolumeDataChannelDescriptor::Components_1, AMPLITUDE_ATTRIBUTE_NAME, "", rangeMin, rangeMax, OpenVDS::VolumeDataMapping::Direct, 1, OpenVDS::VolumeDataChannelDescriptor::Default, 0.f, integerScale, integerOffset));
+
+  OpenVDS::MetadataContainer
+    metadataContainer;
+
+  OpenVDS::VDS *handle = OpenVDS::Create(OpenVDS::InMemoryOpenOptions(), layoutDescriptor, axisDescriptors, channelDescriptors, metadataContainer, error);
+
+  auto accessManager = OpenVDS::GetAccessManager(handle);
+
+  for(int LOD = 0; LOD <= int(lodLevels); LOD++)
+  {
+    const int channel = 0;
+    OpenVDS::VolumeDataPageAccessor* pageAccessor = accessManager.CreateVolumeDataPageAccessor(dimensionality == 2 ? OpenVDS::DimensionsND::Dimensions_01 : OpenVDS::Dimensions_012, LOD, channel, 100, OpenVDS::VolumeDataAccessManager::AccessMode_CreateWithoutLODGeneration);
+    FillPages(pageAccessor, LODTestVoxelValue);
+    accessManager.DestroyVolumeDataPageAccessor(pageAccessor);
+  }
+
+  return handle;
+}
+
+TEST(OpenVDS_integration, TestVolumeDataReadAccessorLOD)
+{
+  for (int fullResDim = -1; fullResDim < 2; ++fullResDim)
+  {
+    OpenVDS::ScopedVDSHandle handle(CreateVolumeDataAccessorLODTestVDS(2, fullResDim));
+    auto accessManager = OpenVDS::GetAccessManager(handle);
+    auto layout = accessManager.GetVolumeDataLayout();
+
+    const int
+      STEPS = 16;
+
+    for (int LOD = 1; LOD <= 4; ++LOD)
+    {
+      auto readAccessor = accessManager.CreateVolumeData2DReadAccessorR32(OpenVDS::DimensionsND::Dimensions_01, LOD, 0);
+      auto interpolatingAccessor = accessManager.CreateVolumeData2DInterpolatingAccessorR32(OpenVDS::DimensionsND::Dimensions_01, LOD, 0, OpenVDS::InterpolationMethod::Cubic);
+
+      int
+        sizeK = layout->GetDimensionNumSamples(0),
+        sizeJ = layout->GetDimensionNumSamples(1),
+        stepK = fullResDim == 0 ? 1 : (1 << LOD),
+        stepJ = fullResDim == 1 ? 1 : (1 << LOD);
+
+      for (int j = 0; j < STEPS; ++j)
+      for (int k = 0; k < STEPS; ++k)
+      {
+        OpenVDS::IntVector2
+          index(j * (sizeJ - 1) / (STEPS - 1),
+                k * (sizeK - 1) / (STEPS - 1));
+
+        float
+          expected = LODTestVoxelValue(OpenVDS::IntVector4(0, 0, index.X & -stepJ, index.Y & -stepK)),
+          actual = readAccessor.GetValue(index);
+
+        if(actual != expected)
+        {
+          EXPECT_EQ(actual , expected);
+          break;
+        }
+
+        OpenVDS::FloatVector2
+          floatIndex((index.X & -stepJ) + stepJ * 0.5f,
+                     (index.Y & -stepK) + stepK * 0.5f);
+
+        if (floatIndex.X < sizeJ && floatIndex.Y < sizeK)
+        {
+          float actualInterpolated = interpolatingAccessor.GetValue(floatIndex);
+          if(actualInterpolated != expected)
+          {
+            EXPECT_EQ(actualInterpolated, expected);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  for (int fullResDim = -1; fullResDim < 3; ++fullResDim)
+  {
+    OpenVDS::ScopedVDSHandle handle(CreateVolumeDataAccessorLODTestVDS(3, fullResDim));
+    auto accessManager = OpenVDS::GetAccessManager(handle);
+    auto layout = accessManager.GetVolumeDataLayout();
+
+    const int
+      STEPS = 6;
+
+    for (int LOD = 1; LOD <= 4; ++LOD)
+    {
+      auto readAccessor = accessManager.CreateVolumeData3DReadAccessorR32(OpenVDS::DimensionsND::Dimensions_012, LOD, 0);
+      auto interpolatingAccessor = accessManager.CreateVolumeData3DInterpolatingAccessorR32(OpenVDS::DimensionsND::Dimensions_012, LOD, 0, OpenVDS::InterpolationMethod::Cubic);
+
+      int
+        sizeK = layout->GetDimensionNumSamples(0),
+        sizeJ = layout->GetDimensionNumSamples(1),
+        sizeI = layout->GetDimensionNumSamples(2),
+        stepK = fullResDim == 0 ? 1 : (1 << LOD),
+        stepJ = fullResDim == 1 ? 1 : (1 << LOD),
+        stepI = fullResDim == 2 ? 1 : (1 << LOD);
+
+      for (int i = 0; i < STEPS; ++i)
+      for (int j = 0; j < STEPS; ++j)
+      for (int k = 0; k < STEPS; ++k)
+      {
+        OpenVDS::IntVector3
+          index(i * (sizeI - 1) / (STEPS - 1),
+                j * (sizeJ - 1) / (STEPS - 1),
+                k * (sizeK - 1) / (STEPS - 1));
+
+        float
+          expected = LODTestVoxelValue(OpenVDS::IntVector4(0, index.X & -stepI, index.Y & -stepJ, index.Z & -stepK)),
+          actual = readAccessor.GetValue(index);
+
+        if(actual != expected)
+        {
+          EXPECT_EQ(actual , expected);
+          break;
+        }
+
+        OpenVDS::FloatVector3
+          floatIndex((index.X & -stepI) + stepI * 0.5f,
+                     (index.Y & -stepJ) + stepJ * 0.5f,
+                     (index.Z & -stepK) + stepK * 0.5f);
+
+        if (floatIndex.X < sizeI && floatIndex.Y < sizeJ && floatIndex.Z < sizeK)
+        {
+          float actualInterpolated = interpolatingAccessor.GetValue(floatIndex);
+          if(actualInterpolated != expected)
+          {
+            EXPECT_EQ(actualInterpolated, expected);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  for (int fullResDim = -1; fullResDim < 4; ++fullResDim)
+  {
+    OpenVDS::ScopedVDSHandle handle(CreateVolumeDataAccessorLODTestVDS(4, fullResDim));
+    auto accessManager = OpenVDS::GetAccessManager(handle);
+    auto layout = accessManager.GetVolumeDataLayout();
+
+    const int
+      STEPS = 4;
+
+    for (int LOD = 1; LOD <= 4; ++LOD)
+    {
+      auto readAccessor = accessManager.CreateVolumeData4DReadAccessorR32(OpenVDS::DimensionsND::Dimensions_012, LOD, 0);
+      auto interpolatingAccessor = accessManager.CreateVolumeData4DInterpolatingAccessorR32(OpenVDS::DimensionsND::Dimensions_012, LOD, 0, OpenVDS::InterpolationMethod::Cubic);
+
+      int
+        sizeK = layout->GetDimensionNumSamples(0),
+        sizeJ = layout->GetDimensionNumSamples(1),
+        sizeI = layout->GetDimensionNumSamples(2),
+        sizeT = layout->GetDimensionNumSamples(3),
+        stepK = fullResDim == 0 ? 1 : (1 << LOD),
+        stepJ = fullResDim == 1 ? 1 : (1 << LOD),
+        stepI = fullResDim == 2 ? 1 : (1 << LOD),
+        stepT = 1; // 1, since dimension 3 is not in dimension group 012.
+
+      for (int t = 0; t < STEPS; ++t)
+      for (int i = 0; i < STEPS; ++i)
+      for (int j = 0; j < STEPS; ++j)
+      for (int k = 0; k < STEPS; ++k)
+      {
+        OpenVDS::IntVector4
+          index(t * (sizeT - 1) / (STEPS - 1),
+                i * (sizeI - 1) / (STEPS - 1),
+                j * (sizeJ - 1) / (STEPS - 1),
+                k * (sizeK - 1) / (STEPS - 1));
+
+        float
+          expected = LODTestVoxelValue(OpenVDS::IntVector4(index.X & -stepT, index.Y & -stepI, index.Z & -stepJ, index.T & -stepK)),
+          actual = readAccessor.GetValue(index);
+
+        if(actual != expected)
+        {
+          EXPECT_EQ(actual , expected);
+          break;
+        }
+
+        OpenVDS::FloatVector4
+          floatIndex((index.X & -stepT) + stepT * 0.5f,
+                     (index.Y & -stepI) + stepI * 0.5f,
+                     (index.Z & -stepJ) + stepJ * 0.5f,
+                     (index.T & -stepK) + stepK * 0.5f);
+
+        if (floatIndex.X < sizeT && floatIndex.Y < sizeI && floatIndex.Z < sizeJ && floatIndex.T < sizeK)
+        {
+          float actualInterpolated = interpolatingAccessor.GetValue(floatIndex);
+          if(actualInterpolated != expected)
+          {
+            EXPECT_EQ(actualInterpolated, expected);
+            break;
+          }
+        }
+      }
+    }
+  }
+}
