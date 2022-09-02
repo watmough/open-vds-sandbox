87fec60ff42de5e9c15063b8b727e0fd6c4cdaba
Jørgen Lind <jorgen.lind@bluware.com>
2022-09-02 10:01:07 +0200

    Compile with clang

--- src/OpenVDS/json_cpp_include.h
+++ src/OpenVDS/json_cpp_include.h
@@ -5,7 +5,16 @@
 #pragma warning(disable:4996)
 #endif
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmacro-redefined"
+#endif
+
 #include <json/json.h>
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 
 #ifdef _MSC_VER
 #pragma warning(pop)
