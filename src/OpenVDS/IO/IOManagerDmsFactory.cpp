396eb6f4f81f4e432b9668eb9fa6643c4de50a8f
Jørgen Lind <jorgen.lind@bluware.com>
2022-11-10 11:52:07 +0100

    Introducing IOManagerDmsProxy
    Adds the posibility to use our own cloud specific IOManagers for seismic
    dms.

--- src/OpenVDS/IO/IOManagerDmsFactory.cpp
+++ src/OpenVDS/IO/IOManagerDmsFactory.cpp
@@ -1,10 +1,15 @@
 #include "IOManagerDmsFactory.h"
 
 #include "IOManagerDms.h"
+#include "IOManagerDmsProxy.h"
+#include "VDS/Env.h"
 namespace OpenVDS
 {
   IOManager* CreateDMSIOManager(const DMSOpenOptions& openOptions, IOManager::AccessPattern accessPattern, Logger &logger, Error& error)
   {
+    bool useCurl = getBooleanEnvironmentVariable("OPENVDS_DMS_CURL");
+    if (useCurl)
+      return new IOManagerDMSProxy(openOptions, accessPattern, logger, error);
     return new IOManagerDms(openOptions, accessPattern, logger, error);
   }
 }
---
+++ src/OpenVDS/IO/IOManagerDmsProxy.h
@@ -0,0 +1,44 @@
+#ifndef IOMANAGERDMSPROXY_H
+#define IOMANAGERDMSPROXY_H
+
+#include <OpenVDS/OpenVDS.h>
+
+#include "IOManager.h"
+#include "IOManagerCurl.h"
+
+#include <memory>
+
+namespace OpenVDS
+{
+  class TokenRefresher;
+  struct DMSManager;
+  struct DMSDataset;
+  struct DMSIOManagerFactory;
+  class IOManagerDMSProxy : public IOManager
+  {
+    public:
+      IOManagerDMSProxy(const DMSOpenOptions &openOptions, IOManager::AccessPattern accessPattern, const Logger &logger, Error &error);
+      ~IOManagerDMSProxy() override;
+
+      std::shared_ptr<Request> ReadObjectInfo(const std::string &objectName, std::shared_ptr<TransferDownloadHandler> handler) override;
+      std::shared_ptr<Request> ReadObject(const std::string &objectName, std::shared_ptr<TransferDownloadHandler> handler, const IORange& range = IORange()) override;
+      std::shared_ptr<Request> WriteObject(const std::string &objectName, const std::string& contentDispostionFilename, const std::string& contentType, const std::vector<std::pair<std::string, std::string>>& metadataHeader, std::shared_ptr<std::vector<uint8_t>> data, std::function<void(const Request & request, const Error & error)> completedCallback = nullptr) override;
+      bool Close(Error& error) override; 
+    private:
+      static std::string AuthProviderCallback(const void* data);
+      DMSOpenOptions m_openOptions;
+      Logger m_logger;
+      CurlHandler m_curlHandler;
+    
+      std::unique_ptr<TokenRefresher> m_tokenRefresher;
+      std::unique_ptr<DMSManager> m_dmsManager;
+      std::unique_ptr<DMSDataset> m_dmsDataset;
+      std::unique_ptr<DMSIOManagerFactory> m_ioManagerFactory;
+      std::unique_ptr<IOManager> m_proxy;
+    
+      bool m_useFileNameForSingleFileDatasets;
+      std::string m_filename;
+  };
+}
+
+#endif
