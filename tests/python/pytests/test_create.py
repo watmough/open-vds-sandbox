c41f53a2772fb91ecf02216e355c216ac6205533
Morten Ofstad <morten.ofstad@bluware.com>
2022-11-29 16:38:40 +0100

    Fix the Python create_1bit_vds so it uses the correct bit-order and check that the result is identical to the original array.

--- tests/python/pytests/test_create.py
+++ tests/python/pytests/test_create.py
@@ -13,7 +13,7 @@ def test_create_1bit_vds():
                                                           0, 0, 4,
                                                           openvds.VolumeDataLayoutDescriptor.LODLevels.LODLevels_None,
                                                           openvds.VolumeDataLayoutDescriptor.Options.Options_None)
-            
+
     axisDescriptors = [ openvds.VolumeDataAxisDescriptor(size[0], "X", "m", 0.0, 2000.0),
                         openvds.VolumeDataAxisDescriptor(size[1], "Y", "m", 0.0, 2000.0),
                         openvds.VolumeDataAxisDescriptor(size[2], "Z", "m", 0.0, 2000.0),
@@ -22,7 +22,7 @@ def test_create_1bit_vds():
                                                                openvds.VolumeDataChannelDescriptor.Components.Components_1,
                                                                "Value", "", 0.0, ((size[2] * 3) * (size[1] * 2) * size[0]) - 1.0)
                          ]
-    
+
     metaData = openvds.MetadataContainer()
 
     vds = openvds.create("inmemory://{}".format(name), "", layoutDescriptor, axisDescriptors, channelDescriptors, metaData)
@@ -35,10 +35,10 @@ def test_create_1bit_vds():
     #        print("shape {} strides {}".format(buf.shape, buf.strides))
             (min, max) = page.getMinMax()
     #        print("min {} max {}".format(min, max))
-            buf[:,:,:] = np.packbits(data[min[2]:max[2],min[1]:max[1],min[0]:max[0]], axis=-1)
+            buf[:,:,:] = np.packbits(data[min[2]:max[2],min[1]:max[1],min[0]:max[0]], axis=-1, bitorder='little')
             page.release()
         accessor.commit()
-    
+
     shape = (layout.getDimensionNumSamples(0), layout.getDimensionNumSamples(1), layout.getDimensionNumSamples(2))
     data = np.random.Generator(np.random.PCG64(seed=313)).integers(2, size=shape[0]*shape[1]*shape[2], dtype=np.bool_).reshape(shape)
 
@@ -46,8 +46,8 @@ def test_create_1bit_vds():
     accessor = manager.createVolumeDataPageAccessor(openvds.DimensionsND.Dimensions_012, 0, 0, 8, openvds.IVolumeDataAccessManager.AccessMode.AccessMode_Create, 1024)
     writePages(accessor, data)
 
-    #req = manager.requestVolumeSubset(min=(0,0,0), max=(shape[0], shape[1], shape[2]))
-    #result = req.data.reshape(shape)
-    #assert np.array_equal(data, result)
-    
+    req = manager.requestVolumeSubset(min=(0,0,0), max=(shape[0], shape[1], shape[2]), format=openvds.VolumeDataFormat.Format_1Bit)
+    result = np.unpackbits(req.data, count=np.prod(shape), bitorder='little').reshape(shape)
+    assert np.array_equal(data, result)
+
     openvds.close(vds)
