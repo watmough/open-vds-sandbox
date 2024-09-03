ac2071fe2c24688456ee91757c4205358853b294
Johan Simon Seland <johan.seland@@bluware.com>
2024-09-03 14:11:45 +0200

    Update to fmt 11.0.2
    This makes OpenVDS combileable with Visual Studio 2022 17.11.
    This version of Visual Studio triggers errors due to the use of (nonstandard) checked iterators.
    Consequently, use of these were removed in later versions of fmt.

--- CMake/BuildFmt.cmake
+++ CMake/BuildFmt.cmake
@@ -1,7 +1,11 @@
 function(BuildFmt)
   set(BUILD_SHARED_LIBS OFF)
   set(CMAKE_CXX_STANDARD 11)
+  if (MSVC)
+    # This avoids propagating the /utf8 command line switch up to targets that depend on fmt.
+	# On other compilers, UTF8 is on enabeled by default.
+	set(FMT_UNICODE OFF)
+  endif()
   add_subdirectory(${fmt_SOURCE_DIR} ${PROJECT_BINARY_DIR}/fmt_${fmt_VERSION} EXCLUDE_FROM_ALL)
   set_target_properties(fmt PROPERTIES FOLDER ExternalProjectTargets/fmt)
-  setCompilerFlagsForTarget(fmt)
 endfunction()
